c87fd0cb64c14c7bca67033aa76e7d53e7917107
glamp <lamp.greg@gmail.com>
2015-04-09 10:36:56 -0400

    things

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
 var yhat = require("./lib/yhat-node")
-var yh = yhat.init("greg", "blah", "yhat-aa02a554-732313416.us-west-1.elb.amazonaws.com")
+var yh = yhat.init("", "", "http://cloud.yhathq.com/")
