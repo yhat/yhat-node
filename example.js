6fcf7addb04b778dd660522d3fde520ca8553b95
glamp <lamp.greg@gmail.com>
2015-06-24 17:09:01 -0400

    new version for http/https

--- example.js
+++ example.js
@@ -1,32 +1,19 @@
-var yhat = require("yhat");
+var yhat = require("./lib/index");
 
 data = {
-    'address_type': 2.5,
-    'building_age': 4.5,
-    'floor_space': 50.5,
-    'floor_space2': 1.5,
-    'has_bathroom': 0.5,
-    'has_central_heating': 1.5,
-    'has_hot_water': 0.5,
-    'kitchen_type': 0.5,
-    'neighborhood_type': 2.0,
-    'rooms': 1.0,
-    'tiled_bathroom': 0.5,
-    'window_type': 0.5,
-    'year_built': 1971.5,
-    'years_on_lease': 2.5
+    name: "Greg"
 }
 
 
-yh = yhat.init("{YOUR USERNAME}",
-               "{YOUR API KEY}",
-               "http://cloud.yhathq.com/");
+yh = yhat.init("{}",
+               "{}",
+               "https://sandbox.yhathq.com/");
 
-yh.predict("rentPredictor", data, function(err, rsp) {
+yh.predict("HolaWorld", data, function(err, rsp) {
     if (err) {
         console.log("Error connecting to server: " + err);
     } else {
-        console.log("this is the result:", JSON.stringify(rsp));
+        console.log("this is the result:", JSON.stringify(rsp, null, 2));
     }
 });
 
